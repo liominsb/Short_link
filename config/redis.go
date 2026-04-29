052660b10d59d5082c4bbcbb41c5074b8a2b387a
weiyuxuan <1753509295@qq.com>
2026-04-29 23:45:13 +0800

    加MQ,优化响应速度

--- config/redis.go
+++ config/redis.go
@@ -12,8 +12,8 @@ func initRedis() {
 		Addr:         Appconf.Database.Addr,
 		Password:     Appconf.Database.Password, // no password set
 		DB:           0,                         // use default DB
-		MinIdleConns: 1,                         //设置最小空闲连接数为3
-		PoolSize:     10,                        //设置连接池大小为10
+		MinIdleConns: 10,                        //设置最小空闲连接数为3
+		PoolSize:     100,                       //设置连接池大小为10
 	})
 
 	_, err := RedisCilnet.Ping().Result()
